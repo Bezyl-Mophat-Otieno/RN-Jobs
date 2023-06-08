faf1a490dc921fc747690cf9363c8c720f118353
BEZYL MOPHAT OTIENO <96384227+Bezyl-Mophat-Otieno@users.noreply.github.com>
2023-06-08 14:31:12 +0300

    app bare bone structure completed

--- app/index.js
+++ app/index.js
@@ -1,53 +1,38 @@
 import React from 'react'
-import { View , Text , StyleSheet, FlatList } from 'react-native'
 import { useState } from 'react'
-import { RefreshControl } from 'react-native'
+import { View , Text , ScrollView , SafeAreaView} from 'react-native'
+import { useRouter , Stack } from 'expo-router'
+import {images ,icons,COLORS,SIZES} from '../constants'
+import {Nearbyjobs , Welcome , Popularjobs , ScreenHeaderBtn } from '../components'
 
-function index() {
-  const [items , setItems ] = useState([
-    {key:1,name:"Joe"},
-    {key:2,name:"MOphat"},
-    {key:3,name:"Kendrick"},
-    {key:4,name:"lamar"},
-    {key:5,name:"Lama"},
-    {key:6,name:"Dev"},
-    {key:7,name:"Java"},
-    {key:8,name:"Html"},
-    {key:9,name:"JS"},
-  ])
-  const [Refresh , setRefresh ] = useState(false)
-  const onRefresh = ()=>{
-  setRefresh(true)
-  setItems([...items,{key:10,name:"React"}])
-  setRefresh(false)
-    
-  }
+
+function Home() {
+  const router = useRouter()
   return (
-
-    <FlatList data={items} keyExtractor={(item , index)=> index.toString()} renderItem={({item})=>
-      <View style={styles.container}>
-        <Text style={styles.text}>{item.name}</Text>
+    <SafeAreaView style={{flex:1,backgroundColor:COLORS.lightWhite}}>
+      <Stack.Screen options={{
+        headerStyle:{backgroundColor:COLORS.lightWhite},
+        headerShadowVisible:false,
+        headerLeft:()=>(
+          <ScreenHeaderBtn iconUrl={icons.menu} dimensions={"60%"}/>
+        )
+        ,
+        headerRight:()=>(
+          <ScreenHeaderBtn iconUrl={icons.profile} dimensions={"100%"}/>
+        ),
+        headerTitle:""
+      }} />
+      <ScrollView showsVerticalScrollIndicator={false}>
+      <View style={{flex:1,padding:SIZES.medium}}>
+        <Welcome/>
+        <Nearbyjobs/>
+        <Popularjobs/>
       </View>
-    } refreshControl={
-      <RefreshControl refreshing={false} onRefresh={onRefresh}colors={["green"]} />
-    } />
-
+      </ScrollView>
       
+    </SafeAreaView>
+
   )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: 'black',
-    padding: 20,
-    margin: 10,
-  },
-  text: {
-    padding: 10,
-    margin: 10,
-    color:"white"
-
-  }
-});
-
-export default index
+export default Home
